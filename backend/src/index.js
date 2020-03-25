525733b786bcffa6760915a23c7324a95872cc4c
Hiago Reis Silva <hiago_reis@hotmail.com>
2020-03-25 20:31:07 -0300

    Backend da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+
+app.listen(3333, ()=>{
+    console.log('listening to port 3333...');
+});
